7e1bf120a2b060fe2a98e837bfd47691e9c7e30c
Phoebe K Sothern <ps01877@srcs.loc>
2021-09-13 15:28:17 -0700

    Milestone 1: Initial Greenfoot Files added

--- Crab.java
+++ Crab.java
@@ -2,12 +2,73 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ * @author Phoebe Sothern
+ * @version 8/26/21
  */
+
 public class Crab extends Actor
 {
+    private int numOfWorms = 0;
+    // This method repeats the following actions
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+    
+    // Turns the Crab at the edge
+    private void turnAtEdge()
+    {
+        if(isAtEdge())
+       {
+           turn(50);
+       }
+    }
+    
+    
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress()
+    {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(4);
+        }
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-4);
+        }
+        if(Greenfoot.isKeyDown("up"))
+        {
+            Greenfoot.delay(1);
+        }
+    }
+
+    
+    // Check for collisions with other objects
+    private void onCollision()
+    {
+       if(isTouching(Worm.class))  
+        {
+            removeTouching(Worm.class);
+            Greenfoot.playSound("slurp.wav");
+            numOfWorms--;
+            
+            // Winning the game
+            if(getWorld().getObjects(Worm.class).size() == 0)
+            {
+                Greenfoot.setWorld(new WinSplash());
+                Greenfoot.playSound("fanfare.wav");
+                Greenfoot.stop();
+            }
+        }
+        
+       if(isTouching(Lobster.class))
+        {
+            Greenfoot.playSound("au.wav");
+            Greenfoot.stop();
+        }
     }
 }
 
--- CrabWorld.java
+++ CrabWorld.java
@@ -9,5 +9,35 @@ public class CrabWorld extends World
     public CrabWorld() 
     {
         super(560, 560, 1);
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab = new Crab();
+        addObject(crab,69,124);
+        crab.setLocation(200,146);
+        Worm worm = new Worm();
+        addObject(worm,415,22);
+        Worm worm2 = new Worm();
+        addObject(worm2,28,266);
+        Worm worm3 = new Worm();
+        addObject(worm3,52,46);
+        Worm worm4 = new Worm();
+        addObject(worm4,498,507);
+        Worm worm5 = new Worm();
+        addObject(worm5,169,430);
+        Worm worm6 = new Worm();
+        addObject(worm6,301,304);
+        Worm worm7 = new Worm();
+        addObject(worm7,359,424);
+        Worm worm8 = new Worm();
+        addObject(worm8,366,138);
+        Lobster lobster = new Lobster();
+        addObject(lobster,388,270);
     }
 }
